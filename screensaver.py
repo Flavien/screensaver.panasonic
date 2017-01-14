d8c11264b28b6df461dc5bc4f4cb5028c86573fa
Flavien Charlon <flavien@charlon.net>
2017-01-14 15:19:45 +0000

    Read the TV hostname from the userdata folder

--- screensaver.py
+++ screensaver.py
@@ -1,3 +1,17 @@
+# Copyright 2016 Flavien Charlon
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import xbmcaddon
 import xbmcgui
 import xbmc
@@ -8,6 +22,7 @@ import xml.etree.ElementTree
 addon = xbmcaddon.Addon()
 addonName = addon.getAddonInfo("name")
 addonPath = addon.getAddonInfo("path")
+addonId = addon.getAddonInfo("id")
 
 class Screensaver(xbmcgui.WindowXMLDialog):
 
@@ -23,7 +38,8 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self.log("Start Screensaver")
         self.exit_monitor = self.ExitMonitor(self.exit)
 
-        rc = panasonic_viera.RemoteControl("192.168.0.250")
+        rc = self.create_remote_control()
+
         rc.turn_off()
 
     def exit(self):
@@ -31,8 +47,13 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self.close()
         self.log("Stop Screensaver")
 
+    def create_remote_control(self):
+        file_path = xbmc.translatePath("special://userdata/addon_data/{addon}/settings.xml".format(addon = addonId))
+        root = xml.etree.ElementTree.parse(file_path).getroot()
+        return panasonic_viera.RemoteControl(root.find("host").text)
+
     def log(self, msg):
-        xbmc.log(u"%(name)s: %(message)s" % {"name": addonName, "message": msg})
+        xbmc.log(u"%(name)s: %(message)s" % {"name": addonId, "message": msg})
 
 if __name__ == "__main__":
     screensaver = Screensaver("screensaver.xml", addonPath, "default")
